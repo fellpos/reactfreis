9e2f96fa0cd6d8c28d2b4c2ada81a470190285e7
fellpos <ra54399754893@acaonsfatima.org.br>
2024-07-31 02:14:02 -0300

    concluido

--- src/pages/exe03/index.jsx
+++ src/pages/exe03/index.jsx
@@ -7,10 +7,25 @@ import { useState } from 'react';
 
 export default function Exe03() {
 
+    const [valorAcaiPeq, SetValorAcaiPeq] = useState(0);
+    const [valorAcaiMed, SetValorAcaiMed] = useState(0);
+    const [valorAcaiGran, SetValorAcaiGran] = useState(0);
+    const [total, SetTotal] = useState(0);
 
-    // function TeclaPressionada(e) {
-    //     if (e.key == 'Enter')
-    // }
+    function conta () {
+        let peq = Number(valorAcaiPeq * 13.50)
+        let med = Number(valorAcaiMed * 15)
+        let gran = Number(valorAcaiGran * 17.50)
+
+        let soma = peq + med + gran
+
+        SetTotal(soma.toFixed(2))
+    }
+
+    function TeclaPressionada(e) {
+        if(e.key == 'Enter')
+        conta()
+    }
 
 
     return (
@@ -62,30 +77,30 @@ export default function Exe03() {
                             <div className='bloco'>
 
                                 <h2><b>Quantidade pequeno</b></h2>
-                                <input type="text" />
+                                <input type="text" value={valorAcaiPeq} onChange={e => SetValorAcaiPeq(e.target.value)}/>
 
                             </div>
 
                             <div className='bloco'>
 
                                 <h2><b>Quantidade médio</b></h2>
-                                <input type="text" />
+                                <input type="text" value={valorAcaiMed} onChange={e => SetValorAcaiMed(e.target.value)}/>
 
                             </div>
 
                             <div className='bloco'>
 
                                 <h2><b>Quantidade grande</b></h2>
-                                <input type="text" />
+                                <input type="text" onKeyUp={TeclaPressionada} value={valorAcaiGran} onChange={e => SetValorAcaiGran(e.target.value)}/>
 
                             </div>
 
 
                         </div>
-                        <button> Executar</button>  
+                        <button onClick={conta}> Executar</button>  
                     </div>
                 </div>
-                <h2 className='res'>Resultado: 0 gramas</h2>
+                <h2 className='res'>Resultado: R$ {total} </h2>
             </section>
         </div>
     );
